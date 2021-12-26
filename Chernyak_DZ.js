77033ca11bd3e5c4fe11d4fa611f71aa02c3d0e4
DimTab <dmitriy.chernyak@gmx.at>
2021-12-26 16:01:16 +0200

    objects with class

--- Chernyak_DZ.js
+++ Chernyak_DZ.js
@@ -1,18 +1,18 @@
-function Store(objname,gender,brand,season,subtype,color,size,material){
-    if(!new.target){
-        return new Store(objname,gender,brand,season,subtype,color,size,material)
+class StoreModel{
+    constructor(objname,gender,brand,season,subtype,color,size,material) {
+        this.objname= objname || ''
+        this.gender= gender || ''
+        this.brand=brand ||''
+        this.season=season || ''
+        this.subtype=subtype ||''
+        this.color=color || ''
+        this.size=size || 0
+        this.material=material ||''
+        }
     }
-    this.objname=objname
-    this.gender=gender
-    this.brand=brand
-    this.season=season
-    this.subtype=subtype
-    this.color=color
-    this.size=size
-    this.material=material
-}
-const jacket = Store(`Jacket`,`m`,`ZARA`,`winter`,`long sleeve`,`white`,`XL`, `wool`)
-const tshirt = Store(`T-shirt`,`f`,`GUCCI`,`summer`,`tiny`,`pink`,`S`, `cotton`)
-console.log(jacket,tshirt)
+const jacket = new StoreModel (`Jacket`,`m`,`ZARA`,`winter`,`long sleeve`,`white`,`XL`, `wool`)
+console.log(jacket)
+const tshirt = new StoreModel(`T-shirt`,`f`,`GUCCI`,`summer`,`tiny`,`pink`,`S`, `cotton`)
+console.log(tshirt)
 
 
