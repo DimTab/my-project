2cfda71ca3c9f7fb27f2ab58204e5b16af7aa550
DimTab <dmitriy.chernyak@gmx.at>
2021-12-30 22:11:01 +0200

    price protection & validation

--- Chernyak_DZ.js
+++ Chernyak_DZ.js
@@ -1,18 +1,47 @@
-class StoreModel{
-    constructor(objname,gender,brand,season,subtype,color,size,material) {
-        this.objname= objname || ''
-        this.gender= gender || ''
-        this.brand=brand ||''
-        this.season=season || ''
-        this.subtype=subtype ||''
-        this.color=color || ''
-        this.size=size || 0
-        this.material=material ||''
+class StoreModel {
+    #price
+    constructor(objname = '', gender = '', size = '', price = 0) {
+        this.setPrice = function (price) {
+            if (/^\d+(\d{1})*(\.\d{1,2})?$/.test(price)) {
+                this.#price = price
+            } else {
+                throw new Error("Incorrect price")
+            }
+        }
+        this.getPrice = function () {
+            return this.#price
+        }
+        this.toString = function () {
+            return `StoreModel: {objname: ${this.objname}, gender: ${this.gender}, size:${this.size}, price: ${this.#price}}`
+        }
+        this.objname = objname
+        this.gender = gender
+        this.size = size
+        if (price !== 0) {
+            this.setPrice(price)
         }
     }
-const jacket = new StoreModel (`Jacket`,`m`,`ZARA`,`winter`,`long sleeve`,`white`,`XL`, `wool`)
-console.log(jacket)
-const tshirt = new StoreModel(`T-shirt`,`f`,`GUCCI`,`summer`,`tiny`,`pink`,`S`, `cotton`)
-console.log(tshirt)
+}
+const jacket = new StoreModel('jacket','m','XL')
+Object.freeze(jacket)
+try{
+    jacket.setPrice(123.78)
+} catch (ex) {
+    console.log (`${ex.message}`)
+}
+// try{
+//     jacket.setPrice('try')
+//     // console.log(jacket)
+// } catch (ex) {
+//     console.log (`Example 2: ${ex.message}`)
+// }
+// try{
+//     jacket.setPrice(-56.67)
+//     // console.log(jacket)
+// } catch (ex) {
+//     console.log (`Example 3: ${ex.message}`)
+// }
+console.log(jacket.toString())
+
 
 
